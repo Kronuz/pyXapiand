d7999a2ea7b365e7973a4114eb18edd6c1900ea9
German M. Bravo <german.mb@deipi.com>
2014-09-04 13:45:51 -0500

    Ready for releasing in PyPI

---
+++ setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import os
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read().strip()
+    except IOError:
+        return ''
+
+setup(
+    name='Xapiand',
+    version='2.0b',
+    author='German M. Bravo (Kronuz)',
+    author_email='german.mb@gmail.com',
+    packages=['xapiand'],
+    url='http://pypi.python.org/pypi/Xapiand/',
+    license='LICENSE.txt',
+    description="Xapian indexing and querying server implemented in Python",
+    long_description=read('README.rst'),
+    install_requires=[
+        "gevent",
+    ],
+)
